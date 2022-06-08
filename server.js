8e2b5daa2d6990affe48b1ade71e942443a92558
rohannayak90 <rohannayak90@gmail.com>
2022-06-08 15:21:44 +0000

    Clock implementation

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// app.configure(function () {
+//   app.use(express.static(__dirname + '/static'));
+// });
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+const port = 4300; // process.env.PORT;
+app.listen(port);
+console.log('Application running successfully on ', port);
